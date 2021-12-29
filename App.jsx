0ae2747a7e8a5b69d5c27bcce8abe4cb3823b81f
Your Name <alexander.klashitsky@gmail.com>
2021-12-29 09:57:28 +0200

    first  commit

---
+++ App.jsx
@@ -0,0 +1,11 @@
+const Router = ReactRouterDOM.HashRouter
+const { Route, Switch } = ReactRouterDOM
+export function App() {
+    return (
+        <Router>
+            <section className="app">
+                fgfgggf
+            </section>
+        </Router>
+    )
+}
---
+++ app.js
@@ -0,0 +1,6 @@
+import { App } from './App.jsx'
+console.log('hi')
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
