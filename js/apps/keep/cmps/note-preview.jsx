9c5bfaa0c3c2f2218f7c92bf9cd98c6ea5ecf7ff
Omer Dahan <omer.dahan26699@gmail.com>
2021-12-31 21:29:03 +0200

    update3

--- js/apps/keep/cmps/note-preview.jsx
+++ js/apps/keep/cmps/note-preview.jsx
@@ -1,3 +1,6 @@
+import { noteService } from "../services/note.service.js";
+
+
 export function NotePreview({ note }) {
   return checkNote(note);
 }
@@ -5,8 +8,7 @@ export function NotePreview({ note }) {
 function checkNote(note) {
   switch (note.type) {
     case "txt":
-      return <p> {note.info.txt}</p>;
-
+      return <p > {note.info.txt}</p>;
     case "img":
       return (
         <React.Fragment>
@@ -19,11 +21,28 @@ function checkNote(note) {
       //   console.log(note.info.todos);
       return (
         <React.Fragment>
-          {/* <h2>{note.info.label}</h2> */}
+          <h2>{note.info.title}</h2>
           {note.info.todos.map((todo) => {
             return <h3>{todo}</h3>;
           })}
         </React.Fragment>
       );
+
+      case"src":
+        let src = note.info.src
+        let idx = src.indexOf('=');
+        let id = src.substring(idx+1,src.length+1);
+      return(
+          <div>
+
+              <iframe width="250" height="200" src={`https://www.youtube.com/embed/${id}`} frameBorder="0"></iframe>
+          </div>
+      )
   }
 }
+
+
+
+
+
+// contentEditable="true" onBlur={()=>noteService.saveNote(note.id,this.value)}
